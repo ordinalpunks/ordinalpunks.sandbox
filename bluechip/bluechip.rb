f23ff3c22c5a48c3b498442dbe84a38f8e15be4c
Gerald Bauer <gerald.bauer@gmail.com>
2023-04-16 19:57:59 +0200

    up

--- bluechip/bluechip.rb
+++ bluechip/bluechip.rb
@@ -7,13 +7,47 @@
 require 'punks'
 
 
+def gen_palette( color )
+  ## from black to color
+  h,s,l = Color.to_hsl( color )
+
+  pp h
+  pp s
+  pp l
+
+  darker = 0.35    ## cut-off colors starting from black
+  lighter = 0.0   ## cut-off colors starting from white
+
+  ldiff = (1.0 - darker - lighter)
+
+  puts "  ldiff: #{ldiff}"
+
+  colors = []
+  256.times do |i|
+     lnew = darker+(ldiff*i / 256.0)
+     puts "  #{i} - #{lnew}"
+     colors << Color.from_hsl( h, s, lnew)
+  end
+
+  colors
+end
+
+
+
+CHIP_BASE    =   Color.from_hex( '#2E358D' )
+CHIP_PALETTE =   gen_palette(  CHIP_BASE )
+
+img = ImagePalette8bit.new( CHIP_PALETTE, size: 4 )
+img.save( "./tmp/palette_chip.png" )
+img.zoom(2).save( "tmp/palette_chipx2.png" )
+
 
 
 CHIP_FRAME   = Image.read( "./bluechip-33x33.png" )
 puts "   #{CHIP_FRAME.width}x#{CHIP_FRAME.height}"
 
 
-def mint( punk )
+def old_mint( punk )
   ## change to coin color palette
   chip = Image.new( 33, 33 )
   chip.compose!( Image.new( 24, 24, '#ffffff'), 6, 3 )
@@ -22,6 +56,14 @@ def mint( punk )
   chip
 end
 
+def mint( punk )
+  ## change to coin color palette
+  chip = Image.new( 33, 33 )
+  chip.compose!( Image.new( 22, 22, CHIP_BASE ), 6, 6 )
+  chip.compose!( punk.change_palette8bit( CHIP_PALETTE.reverse ), 6, 3 )
+  chip.compose!( CHIP_FRAME )
+  chip
+end
 
 
 

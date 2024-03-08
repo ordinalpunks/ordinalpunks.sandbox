a17e9f1eeb28ca3d2ad73c37e2078ee312686362
Gerald Bauer <gerald.bauer@gmail.com>
2024-03-08 15:33:31 +0100

    up

---
+++ mogs/mogs.rb
@@ -0,0 +1,88 @@
+#####
+#  to run use:
+#    $   ruby ./mogs.rb
+
+
+require 'punks'
+
+
+def mog( *attributes, id )
+  base            = attributes[0]
+  more_attributes = attributes[1..-1]
+
+  ## quick fix:  frown not supported for now on female gold
+  more_attributes = more_attributes.reject do |attr|
+                                            ['Big Shades',
+                                             '3D Glasses',
+                                             'VR',
+                                             'Clown Eyes Green',
+                                             'Clown Eyes Blue',
+                                             'Regular Shades',
+                                             'Eye Patch',
+                                             'Eye Mask',
+                                             'Classic Shades',
+                                             'Nerd Glasses',
+                                             'Blue Eyeshadow',
+                                             'Green Eye Shadow',
+                                             'Heart Shades',
+                                             'Laser Eyes',
+                                            ].include?(attr)
+                                           end
+
+  punk = Punk::Image.generate( base, *more_attributes )
+  punk
+
+  if base.downcase.index( 'female')
+    punk.compose!( Image.read( './more/mog-female.png') )
+  else
+    punk.compose!( Image.read( './more/mog-male.png') )
+  end
+  punk
+end
+
+
+####
+#  read in ordinals metadata
+recs = read_csv( "../ordinalpunks_v2.csv" )
+puts "    #{recs.size} record(s)"
+
+
+def rec_to_attributes( rec )
+  type =     rec['type']
+  gender =   rec['gender']
+  skin_tone = rec['skin_tone']
+
+  # note: merge type+gender+skin_tone into one attribute
+  base = "#{type} #{gender}"
+  base << " #{skin_tone}"       unless skin_tone.empty?
+
+  accessories = rec['accessories'].split( '/' ).map { |acc| acc.strip }
+  attributes = [base] + accessories
+  attributes
+end
+
+
+
+composite     = ImageComposite.new( 10, 10 )
+
+ids = (0..99)
+pp ids
+
+ids.each do |id|
+  attributes = rec_to_attributes( recs[id] )
+  pp attributes
+
+  punk = mog( *attributes, id )
+
+  punk.save( "./tmp2/mog-#{id+1}.png" )
+  punk.zoom(4).save( "./tmp2/mog-#{id+1}@4x.png" )
+  punk.zoom(8).save( "./tmp2/mog-#{id+1}@8x.png" )
+
+  composite << punk
+end
+
+
+composite.save( "./tmp/mogs.png" )
+composite.zoom(4).save( "./tmp/mogs@4x.png" )
+
+puts "bye"
